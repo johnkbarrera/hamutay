79dfa3f4537737142386091eda29869aeea36b8b
jk.barrera <johnkevinbarrera@gmail.com>
2026-04-06 15:57:19 -0500

    feat: add ecosystem configuration for PM2 process management

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  apps: [
+    {
+      name: 'hamutay_frontend',
+      script: 'serve',
+      args: '-s dist -l 3000',
+      env: {
+        NODE_ENV: 'production',
+      },
+      instances: 1,
+      exec_mode: 'cluster',
+      watch: false,
+      max_memory_restart: '500M',
+      error_file: './pm2_logs/error.log',
+      out_file: './pm2_logs/out.log',
+      log_file: './pm2_logs/combined.log',
+      time: true,
+      merge_logs: true,
+      autorestart: true,
+      max_restarts: 10,
+      min_uptime: '10s',
+    },
+  ],
+};
